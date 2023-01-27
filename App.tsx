2fc07ed5264384369dd0296a8e45720d8e137faa
Nikola Dlubacz <nikola.dlubacz@capgemini.com>
2023-01-27 12:16:57 +0100

    Customize header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer} from "@react-navigation/native";
-import { createNativeStackNavigator} from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import DrinkList from "./screens/DrinkList";
 import { RootStackParamList } from "./util/types";
+import Colors from "./constants/colors";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,11 +14,22 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.grey800 },
+            headerTintColor: Colors.green200,
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontSize:30
+            },
+          }}
+        >
           <Stack.Screen
             name="WelcomeScreen"
             component={WelcomeScreen}
-            options={{ title: "Welcome" }}
+            options={{
+              title: "Drink Advisor",
+            }}
           />
           <Stack.Screen name="DrinkList" component={DrinkList} />
         </Stack.Navigator>
--- constants/colors.ts
+++ constants/colors.ts
@@ -1,6 +1,11 @@
-const Colors ={
-    primary500: "#616161",
-    primary300: "#cccccc"
-}
+const Colors = {
+  primary500: "#616161",
+  primary300: "#cccccc",
+  grey800:"#3F3F3F",
+  green800: "#698474",
+  green600: "#889E81",
+  green400: "#BAC7A7",
+  green200: "#E5E4CC",
+};
 
 export default Colors;
